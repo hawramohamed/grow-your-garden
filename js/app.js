37794ef407643d67ed1d7b37a19446b83ef6ce9f
hawra <hawra.yusuf02@outlook.com>
2025-08-16 11:58:12 +0300

    seeds dropdown hidden within seeds btn + water timer done

--- js/app.js
+++ js/app.js
@@ -3,23 +3,74 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-let state = {
-    boredom: 0,
-    hunger: 0,
-    sleepiness: 0,
+let stat = {
+    water: 0,
 };
 
 let timer;
 let gameOver;
 
 /*------------------------ Cached Element References ------------------------*/
-const seedsBtn = document.getElementById('seeds-btn');
-const seedDropdown = document.getElementById('seed-dropdown');
+const seedsBtnEl = document.getElementById('seeds-btn');
+const waterBtnEl = document.querySelector('#water-btn');
 
+const seedDropdownEl = document.getElementById('seed-dropdown');
 
+const waterStatEl = document.querySelector('#water-stat');
 
+const gameLoseMsgEl = document.querySelector('#message');
+
+const resetBtnEl = document.querySelector('#restart');
 /*-------------------------------- Functions --------------------------------*/
+const gameStart = () => {
+    waterStatUpdate();
+    checkWaterLevel();
+    render();
+};
 
+const waterStatUpdate = () => {
+    stat.water += Math.floor(Math.random()*3);
+};
+
+const checkWaterLevel = () => {
+    if(stat.water >= 10){
+        gameOver = true;
+    };
+};
+
+const render = () => {
+    waterStatEl.textContent = stat.water;
+
+    if(gameOver){
+        resetBtnEl.classList.remove("hidden");
+        gameLoseMsgEl.classList.remove("hidden");
+        clearInterval(timer);
+    }
+};
+
+const waterBtnClick = () => {
+    stat.water = 0;
+    render();
+};
+
+const inital = () => {
+    resetBtnEl.classList.add("hidden");
+    gameLoseMsgEl.classList.add("hidden");
+    stat.water = 0;
+    gameOver = false;
+    timer = setInterval(gameStart, 2000);
+}
+
+inital();
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+seedsBtnEl.addEventListener('click', () => {
+    if (seedDropdownEl.style.display === 'none'){
+        seedDropdownEl.style.display = 'block';
+    } else{
+        seedDropdownEl.style.display = 'none';
+    }
+});
+
+waterBtnEl.addEventListener('click', waterBtnClick);
