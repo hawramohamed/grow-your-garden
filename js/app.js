79b0500fb2ca05e7de7b5d063e8b29978c002fd5
hawra <hawra.yusuf02@outlook.com>
2025-08-16 10:59:49 +0300

    rough html created, assets added, and js started

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+let state = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0,
+};
+
+let timer;
+let gameOver;
+
+/*------------------------ Cached Element References ------------------------*/
+const seedsBtn = document.getElementById('seeds-btn');
+const seedDropdown = document.getElementById('seed-dropdown');
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
