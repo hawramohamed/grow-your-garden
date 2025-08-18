55f25ab8308b0ff08b19830fa4030cc7b9f7bba8
hawra <hawra.yusuf02@outlook.com>
2025-08-18 19:12:07 +0300

    added harvest seeds logic and stats js

--- js/app.js
+++ js/app.js
@@ -7,15 +7,20 @@ let stat = {
     water: 0,
 };
 
+let harvestedPlants = [];
+
 let timer = 0;
 let gameOver;
 let waterClickCount = 0;
-let seedChosen = false;
 let seedsClickCount = 0;
+let harvestClickCount = 0;
+let seedChosen = false;
+
 /*------------------------ Cached Element References ------------------------*/
 
 const seedsBtnEl = document.querySelector('#seeds-btn');
 const waterBtnEl = document.querySelector('#water-btn');
+const harvestBtnEl = document.querySelector('#harvest-btn');
 
 const seedDropdownEl = document.querySelector('#seed-dropdown');
 
@@ -61,7 +66,9 @@ const growthUpdate = () => {
     strawberryImage.classList.add("hidden");
     lettuceImage.classList.add("hidden");
 
+    //if statement for images    
     if(waterClickCount < 5){
+        console.log("unhiding")
         seedImage.classList.remove("hidden");
     } else if (waterClickCount < 8){
         growthImage.classList.remove("hidden");
@@ -71,8 +78,47 @@ const growthUpdate = () => {
         if (finalImage){
             finalImage.classList.remove("hidden");
         };
-
     };
+
+    //if statement to disable harvest button
+    if(waterBtnClick < 10){
+        harvestBtnEl.disabled = true;
+    } else {
+        harvestBtnEl.disabled = false;
+    }
+};
+
+const harvestBtnClick = () => {
+    if (waterClickCount >= 10){
+        const harvestedSeed = seedDropdownEl.value;
+        harvestedPlants.push(harvestedSeed);
+
+        harvestClickCount ++;
+        harvestStatEl.textContent = harvestClickCount;
+
+        //reset progress for new plant
+        waterClickCount = 0;
+        stat.water = 0;
+        waterStatEl.textContent = stat.water;
+        harvestBtnEl.disabled = true;
+
+        //stop water thirst 
+        clearInterval(timer);
+        timer = 0;
+
+        console.log("hiding")
+        //remove plants after harvest
+        seedImage.classList.add("hidden");
+        growthImage.classList.add("hidden");
+        sunflowerImage.classList.add("hidden");
+        strawberryImage.classList.add("hidden");
+        lettuceImage.classList.add("hidden");
+
+        //choose new plant
+        seedChosen = false;
+        seedDropdownEl.classList.remove("hidden");  
+    };
+    
 };
 
 const render = () => {
@@ -115,6 +161,7 @@ const inital = () => {
     stat.water = 0;
     waterClickCount = 0;
     seedsClickCount = 0;
+    harvestClickCount = 0;
     gameOver = false;
 
     clearInterval(timer);
@@ -132,6 +179,7 @@ const inital = () => {
 
     waterStatEl.textContent = stat.water;
     seedStatEl.textContent = seedsClickCount;
+    harvestStatEl.textContent = harvestClickCount;
 };
 
 inital();
@@ -142,5 +190,7 @@ seedsBtnEl.addEventListener('click', dropdownDisplay);
 seedDropdownEl.addEventListener('change', dropdownChosen);
 
 waterBtnEl.addEventListener('click', waterBtnClick);
+
+harvestBtnEl.addEventListener('click', harvestBtnClick);
 
 resetBtnEl.addEventListener('click', inital);
